27b3293498e292b8f879066d605115cb34fceea4
Ryan Gilmartin <ryan22gil@gmail.com>
2024-04-07 18:39:01 -0400

    fixed styling added table-view

---
+++ src/app/lib/food_temp.ts
@@ -0,0 +1,20 @@
+import { Food, Can } from "./definitions";
+
+export const food: Can[] = [
+  {
+    name: "Nulo Chicken and turkey 5.5oz",
+    cal_kg: 1239,
+    gr_can: 156,
+    price: 1.85,
+    currency: "USD",
+    value_rating: 5,
+  },
+  {
+    name: "Nulo Beef and Lamb 5.5oz",
+    cal_kg: 1457,
+    gr_can: 156,
+    price: 2.29,
+    currency: "USD",
+    value_rating: 4,
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Calculator from "./ui/calculator";
+import FoodTable from "./ui/food_db";
 
 export default function Home() {
   return (
     <div className="">
-      <div className="bg-teal-900 h-48">
+      <div className="bg-teal-900 min-h-48">
         <h1 className="p-5 text-3xl text-slate-50">Wet cat food calculator</h1>
-        <p className="px-20 text-slate-50">
+        <p className="px-20 py-5 text-slate-50">
           This Website provides a simple way for you to compare the costs of
           food items for your cat. Everyone wants to give their cat the best
           food possible, but it can be hard to know what is best for your cat
@@ -15,7 +16,14 @@ export default function Home() {
           decision.
         </p>
       </div>
-      <Calculator />
+
+      {/* Flexbox to hold the Calculator and database */}
+      <div className="flex flex-row my-5">
+        <div className="bg-white rounded-md drop-shadow-md p-5 my-0 mx-5">
+          <Calculator />
+        </div>
+        <FoodTable />
+      </div>
     </div>
   );
 }
