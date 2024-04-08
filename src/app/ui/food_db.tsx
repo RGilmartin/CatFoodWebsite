96a4ea03c493c95487eea480e14daf15cc6f6b03
Ryan Gilmartin <ryan22gil@gmail.com>
2024-04-07 22:37:42 -0400

    added extended can type for table to show fns

--- src/app/ui/food_db.tsx
+++ src/app/ui/food_db.tsx
@@ -6,8 +6,11 @@ import {
 } from "material-react-table";
 import { Can } from "../lib/definitions";
 import { useEffect, useMemo, useState } from "react";
+import { cansPerDay, costPerMonth } from "../lib/can_calc";
 const FoodTable = () => {
   const [food, setFood] = useState([]); //Food data from api
+
+  type InfoCan = Can & { cans_per_day: number; cost_per_month: number };
 
   // Get the data for the table
   useEffect(() => {
@@ -15,36 +18,60 @@ const FoodTable = () => {
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setFood(data);
+        let cansInfo: InfoCan[] = [];
+        data.forEach((can: Can) => {
+          let cpd = cansPerDay(210, can); // TODO, add changable target Cals
+          let cpm = costPerMonth(210, can);
+          cansInfo.push({ ...can, cans_per_day: cpd, cost_per_month: cpm });
+        });
+        setFood(cansInfo);
       });
   }, []);
 
   // Generate columns for the table
-  const columns = useMemo<MRT_ColumnDef<Can>[]>(
+  const columns = useMemo<
+    MRT_ColumnDef<Can & { cans_per_day: number; cost_per_month: number }>[]
+  >(
     () => [
       {
         header: "Name",
         accessorKey: "name",
+        size: 150,
       },
       {
         header: "Price",
         accessorKey: "price",
+        size: 75,
       },
       {
         header: "Value Rating",
         accessorKey: "value_rating",
+        size: 50,
       },
       {
         header: "Cals/KG",
         accessorKey: "cal_kg",
+        size: 75,
       },
       {
         header: "Grams/Can",
         accessorKey: "gr_can",
+        size: 75,
       },
       {
         header: "Currency",
         accessorKey: "currency",
+        size: 50,
+      },
+      {
+        header: "Cans/Day",
+        accessorKey: "cans_per_day",
+        size: 75,
+      },
+      {
+        header: "Cost/Month",
+        accessorKey: "cost_per_month",
+        size: 75,
       },
     ],
     [],
